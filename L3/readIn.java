abd6079b4203d3eecf1f295df22b2db91f8f08b5
Varun Ved <varunmved@gmail.com>
2015-08-02 11:05:57 -0700

    fixed some issues + implemented test case for L2

---
+++ L3/readIn.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class readIn
+{
+  public static void main(String[] args){
+    Scanner b = new Scanner(System.in);
+    String input = null;
+    //input = input.toLowerCase();
+    while (input.contains("done:") != true)
+    {
+      input = b.nextLine();
+      switch (input)
+      {
+
+        case (input.contains("const") == true):
+        //push p
+        break;
+        case (input.contains("load") == true):
+        string[] lineInput = input.split(" ");
+        int varIn = int(lineInput[2]);
+        break;
+        case (input.contains("sto") == true):
+        string[] lineInput = input.split(" ");
+        int varIn = int(lineInput[2]);
+        break;
+        case (input.contains("add") == true):
+        /*
+        y = pop() x = pop() d = x+ y push(d)
+        */
+        break;
+        case (input.contains("sub") == true):
+        /*
+        y = pop() x = pop() d = x- y push(d)
+        */
+        break;
+        case (input.contains("mul") == true):
+        /*
+        y = pop() x = pop() d = x* y push(d)
+        */
+        break;
+        case (input.contains("div") == true):
+        /*
+        y = pop() x = pop() d = x/ y push(d)
+        */
+        break;
+        case (input.contains("eql") == true):
+        /*
+        y = pop()
+        x = pop()
+        if x == y push(1) else push(0)
+        */
+        break;
+        case (input.contains("lss") == true):
+        /*
+        y = pop()
+        x = pop()
+        if x < y push(1) else push(0)
+        */
+        break;
+        case (input.contains("gtr") == true):
+        /*
+        y = pop()
+        x = pop()
+        if x > y push(1) else push(0)
+        */
+        break;
+        case (input=="f"):
+        string[] lineInput = input.split(" ");
+        int varIn = int(lineInput[2]);
+        break;
+
+
+
+      }
+
+
+    }
+  }
+}
---
+++ l2/ArrayStack2.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+interface StackIntADT {
+ public void push(int element);
+ public int pop();
+ public int peek();
+ public boolean isEmpty();
+ public boolean isFull();
+ public int size();
+}
+
+
+public class ArrayStack implements StackIntADT
+{
+ // fields
+ private static int DEFAULT_SIZE = 10;
+ private int stackPointer;
+ private int[] myStack;
+ private int size;
+
+ // constructors
+ public ArrayStack()
+ {
+   myStack = new int[DEFAULT_SIZE];
+   stackPointer = 0;
+ }
+
+ public ArrayStack(int size)
+ {
+   if(size > 0)
+   {
+     myStack = new int[size];
+     stackPointer = 0;
+   }
+ }
+
+ // stack class methods
+ //add an element to the stack
+ public void push(int element)
+ {
+   //if(stackPointer == myStack.length)
+   /*
+   if(isFull()==true)
+   {
+       System.out.println("isfull");
+   }
+   */
+   stackPointer++;
+   //else
+   myStack[stackPointer]=element;
+ }
+
+ //take an element off the stack
+ public int pop() {
+   if(isEmpty()==true)
+   {
+     System.exit(0);
+
+   }
+   return(stackPointer--);
+
+ }
+
+ //access the element specified
+ public int peek() {
+   if (stackPointer == -1)
+   {
+     return -1;
+
+   }
+   else return myStack[stackPointer];
+
+ }
+
+
+ //check if the stack is empty
+ public boolean isEmpty()
+ {
+   return (stackPointer == -1);
+ }
+
+ //check if the stack is full
+ public boolean isFull()
+ {
+   //if(stackPointer==size-1) return true;
+   //else
+   return false;
+
+ }
+ public int size()
+ {
+   return myStack.length;
+ }
+
+ public static void main(String[] args)
+ {
+   Scanner b = new Scanner(System.in);
+   StackIntADT ASImp;
+
+   System.out.println("Enter a size > 0, or enter -1 if you want the default size of 10\n");
+   int sizeIn = b.nextInt();
+   if (sizeIn == -1)
+     {
+        ASImp = new ArrayStack();
+     }
+   else
+   {
+      ASImp = new ArrayStack(sizeIn);
+   }
+   ASImp.push(1);
+   ASImp.push(2);
+   ASImp.push(3);
+   ASImp.push(4);
+   boolean isFullRes = ASImp.isFull();
+   System.out.println(isFullRes);
+   int val = ASImp.pop();
+   System.out.println(val);
+
+ }
+
+
+}
